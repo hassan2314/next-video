3323162b597d99209f89206aa76b2a38f729c88d
Hassan Ahmed <hassan87691@gmail.com>
2025-08-25 21:49:36 +0500

    Provider and Layout

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,3 +1,12 @@
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
+
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <SessionProvider refetchInterval={5 * 60}>
+      <ImageKitProvider urlEndpoint={urlEndpoint}>{children}</ImageKitProvider>
+    </SessionProvider>
+  );
 }
