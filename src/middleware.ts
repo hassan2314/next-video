d68e922a833b2134898f03a85f877d1a0d5e10ad
Hassan Ahmed <hassan87691@gmail.com>
2025-08-25 20:06:28 +0500

    route add

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,38 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  // Matches the pages config in `[...nextauth]`
-  pages: {
-    signIn: "/login",
-    error: "/error",
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
   },
-});
+  {
+    callbacks: {
+      async authorized({ req, token }) {
+        const { pathname } = req.nextUrl;
+        if (
+          pathname.startsWith("/api/auth") ||
+          pathname === "/login" ||
+          pathname === "/register"
+        )
+          return true;
+        if (pathname.startsWith("/") || pathname === "/api/video") return true;
+
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/data (SSG data cache)
+     * - public files (public folder)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/data|public|favicon.ico).*)",
+  ],
+};
